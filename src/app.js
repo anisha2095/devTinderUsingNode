37444c552b525c050a0d4311024c9e1478f88834
KUMARI <anisha.f.kumari@capgemini.com>
2025-06-14 18:56:14 +0530

    create schema and post the user data to signup api using dummy data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
-const app = express();
-const {adminAuth} = require("./middleware/Auth")
 const connectedDB = require("./config/database");
+const app = express();
+const User = require("./models/user")
 
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Ankita",
+        lastName: "Kumari",
+        emailId: "ankita@gmail.com",
+        password: "ankita333"
+    })
+
+    try {
+        await user.save();
+        res.send("user successfully saved");
+    } catch(err){
+        res.status(400).send(err.message)
+    }
+})
 
 //DB Connection
 connectedDB().then(() => {
@@ -15,24 +29,5 @@ connectedDB().then(() => {
     console.log("Database is not connected")
 })
 
-
-//Middleware exapmple
-app.use("/", (req, res, next )=> {
-    console.log("/ router")
-    next()
-})
-app.use("/user", (req, res )=> {
-    console.log("user router")
-    res.send("user router")
-})
-app.use("/admin", adminAuth) //admin router applicable for all admjin get and admin delete post patch
-app.get("/admin/getAllAdmin", (req, res )=> {
-    console.log("admin added successfully")
-    res.send("admin added successfully")
-})
-app.delete("/admin/deleteAdmin", (req, res )=> {
-    console.log("Deleted admin successfully")
-    res.send("Deleted admin successfully")
-})
 
 
