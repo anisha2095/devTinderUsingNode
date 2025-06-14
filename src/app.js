f244421327c7177ae1310f0379cba984eb9b589e
KUMARI <anisha.f.kumari@capgemini.com>
2025-06-14 14:14:21 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,43 @@ const express = require("express");
 
 const app = express();
 
-//Multiple Router handler
+//Middleware
 
 // 1st scenario
+// app.use("/", (req,res) => {
+//     console.log("first / router")
+// })
 // app.get("/user", (req, res) => {
 //     console.log("response1")
 //     res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
 // },
 // (req,res) => {
 //     console.log("response2")
-//     res.send({firstName: "ankita"})
 // }) 
-//output - response1
+//output - first / router
+//and in postman
+//http://localhost:4000/user
+//loop keep going because all the time "/"" router executes and dont have response, user router did not execute because "/"" it is placed above user router and it is always applicable for all routes so we have to make sure about sequence
+//end
+
+// 2nd scenario
+// app.use("/", (req,res, next) => {
+//     console.log("first / router")
+//     next();
+// })
+// app.get("/user", (req, res, next) => {
+//     console.log("response1")
+//     next();
+// },
+// (req,res, next) => {
+//     console.log("response2")
+//     next();
+// },
+// (req,res) => {
+//     console.log("response3")
+//     res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
+// }) 
+//output - first / router response1 response2 response3
 //and in postman
 //http://localhost:4000/user
 // {
@@ -22,84 +47,56 @@ const app = express();
 // }
 //end
 
-// 2nd scenario - to execute 2nd handler we will use next function
-// app.get("/user", (req, res,  next) => {
-//     console.log("response1")
-//     // res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
-//     next();
-// },
-// (req,res) => {
-//     console.log("response2")
-//     res.send({firstName: "ankita"})
-// })
-//output - response1  response2
-//and in postman
-//http://localhost:4000/user
-// {
-//     "firstName": "ankita",
-// }
-//end
-
-// 3rd scenario - sending response from 1st scenario and also using next function
-// app.get("/user", (req, res,  next) => {
-//     console.log("response1")
+// 3rd scenario
+// app.use("/", (req,res, next) => {
+//     console.log("first / router")
 //     res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
 //     next();
+// })
+// app.get("/user", (req, res, next) => {
+//     console.log("response1")
+//     next();
+// },
+// (req,res, next) => {
+//     console.log("response2")
+//     next();
 // },
 // (req,res) => {
-//     console.log("response2")
-//     res.send({firstName: "ankita"})
-// })
-//output - response1  response2  error
-// "error" because user route we already sending a response. so, from the 2nd handler while we sending it gives and error becz we can send only one rsponse
+//     console.log("response3")
+// }) 
+//output - first / router response1 response2 response3
 //and in postman
 //http://localhost:4000/user
 // {
-//         "firstName": "Anisha",
-//         "lastName": "Kumari"
+//     "firstName": "Anisha",
+//     "lastName": "Kumari"
 // }
-
 //end
 
-// 4th scenario - sending response from 1st scenario and also using next function
-// app.get("/user", (req, res,  next) => {
-//     console.log("response1")
-//     // res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
-//     next();
-// },
-// (req,res) => {
-//     console.log("response2")
-//     // res.send({firstName: "ankita"})
-// })
-//output - response1  response2
+// 4th scenario
+app.use("/", (req,res, next) => {
+    console.log("first / router")
+    next();
+    res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
+})
+app.get("/user", (req, res, next) => {
+    console.log("response1")
+    next();
+},
+(req,res, next) => {
+    console.log("response2")
+},
+(req,res) => {
+    console.log("response3")
+}) 
+//output - first / router response1 response2 
 //and in postman
 //http://localhost:4000/user
-//no response, and continously loop shown
+// {
+//     "firstName": "Anisha",
+//     "lastName": "Kumari"
+// }
 //end
-
-// 5th scenario - both the router handler having only next function, no response
-// app.get("/user", (req, res,  next) => {
-//     console.log("response1")
-//     // res.send({firstName: "Anisha", lastName: "Kumari", usercity: req.query.usercity})
-//     next();
-// },
-// (req,res) => {
-//     console.log("response2")
-//     // res.send({firstName: "ankita"})
-//     next();
-// })
-//output - response1  response2 error
-// error because last handler having next function but we dont have next handler 
-//and in postman
-//http://localhost:4000/user
-//error - 500 internal server, defined next function in last handler but there is no router handler exist.
-//end
-
-
-//Like this we can have multiple router handler, only one response we can send if already send the next handler have response then it will give an error 
-//If we using next function means it runs the next router handler. if router handler not exist then it given an error in postman and terminal both
-//use method applicable for all http methods - get,post,patch,delete
-//or we can use methods seperatly get,post,patch,delete
 
 app.listen(4000, () => {
     console.log("server is ready")
