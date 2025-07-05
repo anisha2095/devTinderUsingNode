eed778dafa684c7c4e88296f8c67fd4ad6899042
KUMARI <anisha.f.kumari@capgemini.com>
2025-07-05 17:02:04 +0530

    all schema validation, custom validation function for gender, timestamps

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,52 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
     },
     lastName: {
         type: String
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+    },
+    about: {
+        type: String, 
+        default: "This is default about of a user"
+    },
+    skills: {
+        type: [String]
     }
+},
+
+{
+    timestamps: true
 });
 
 const User = mongoose.model("User", userSchema);
